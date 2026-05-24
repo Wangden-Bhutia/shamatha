fc86280573196ca1ed5038e86d476670106d6205
Wangden-Bhutia <142923087+Wangden-Bhutia@users.noreply.github.com>
2026-05-24 14:22:22 +0530

    MVP production release prep

--- android/app/src/main/java/com/stillmindlabs/shamatha/MainActivity.java
+++ android/app/src/main/java/com/stillmindlabs/shamatha/MainActivity.java
@@ -1,5 +1,24 @@
 package com.stillmindlabs.shamatha;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setBackgroundColor(
+                Color.parseColor("#050816")
+        );
+
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().setBackgroundColor(
+                    Color.parseColor("#050816")
+            );
+        }
+    }
+}
